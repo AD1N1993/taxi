9f4137f861cf3dfd3478d927ab481ef8389ae04c
yurygrivickiy <yury.grivickiy@finespace.com>
2026-07-03 09:59:59 +0300

    feat: Add driver management API and configure ESLint/Prettier

--- src/setup-app.ts
+++ src/setup-app.ts
@@ -1,11 +1,59 @@
-import express, { Express } from "express";
- 
+import express, { Express, Request, Response } from 'express';
+import { db } from './db/in-memory.db';
+import { DriverInputDto } from './drivers/dto/driver.input-dto';
+import { Driver } from './drivers/types/driver';
+import { HttpStatus } from './core/types/http-statuses';
+
 export const setupApp = (app: Express) => {
-  app.use(express.json()); // middleware для парсинга JSON в теле запроса
- 
-  // основной роут
-  app.get("/", (req, res) => {
-    res.status(200).send("Hello world!");
+  app.use(express.json());
+
+  app.get('/', (req, res) => {
+    res.status(200).send('Hello world!');
   });
+
+  app.get('/drivers', (req: Request, res: Response) => {
+    res.status(200).send(db.drivers);
+  });
+
+  app.get('/drivers/:id', (req: Request<{ id: string }>, res: Response) => {
+    const driver = db.drivers.find(
+      (driver) => driver.id === parseInt(req.params.id, 10),
+    );
+    if (!driver) {
+      res.status(404).send('Driver not found');
+    }
+    res.status(200).send(driver);
+  });
+
+  app.post(
+    '/drivers',
+    (req: Request<{}, {}, DriverInputDto>, res: Response) => {
+      const newDriver: Driver = {
+        id: db.drivers.length ? db.drivers[db.drivers.length - 1].id + 1 : 1,
+        name: req.body.name,
+        phoneNumber: req.body.phoneNumber,
+        email: req.body.email,
+        vehicleMake: req.body.vehicleMake,
+        vehicleModel: req.body.vehicleModel,
+        vehicleYear: req.body.vehicleYear,
+        vehicleLicensePlate: req.body.vehicleLicensePlate,
+        vehicleDescription: req.body.vehicleDescription,
+        vehicleFeatures: req.body.vehicleFeatures,
+        createdAt: new Date(),
+      };
+      db.drivers.push(newDriver);
+      res.status(HttpStatus.Created).send(newDriver);
+    },
+  );
+
+  app.get('/testing', (req: Request, res: Response) => {
+    res.status(200).send('testing url');
+  });
+
+  app.delete('/testing/all-data', (req: Request, res: Response) => {
+    db.drivers = [];
+    res.sendStatus(HttpStatus.NoContent);
+  });
+
   return app;
 };
