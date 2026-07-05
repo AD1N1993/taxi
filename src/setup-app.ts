0e71cfa16d0995c91199437341bc3ee3a73393de
yurygrivickiy <yury.grivickiy@finespace.com>
2026-07-05 22:27:08 +0300

    feat(swagger): Integrate Swagger for API documentation and update routes to include API prefix

--- src/setup-app.ts
+++ src/setup-app.ts
@@ -1,6 +1,7 @@
 import express, { Express } from 'express';
 import { driversRouter } from './drivers/routers/drivers.router';
 import { testingRouter } from './testing/routers/testing.router';
+import { setupSwagger } from './core/swagger/setup-swagger';
 
 export const setupApp = (app: Express) => {
   app.use(express.json());
@@ -9,8 +10,10 @@ export const setupApp = (app: Express) => {
     res.status(200).send('Hello world!');
   });
 
-  app.use('/drivers', driversRouter);
-  app.use('/testing', testingRouter);
+  app.use('/api/drivers', driversRouter);
+  app.use('/api/testing', testingRouter);
+
+  setupSwagger(app);
 
   return app;
 };
