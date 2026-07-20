5ff1a46aeae6cc900ecc54b3d99b0a2cc89d6267
yurygrivickiy <yury.grivickiy@finespace.com>
2026-07-20 09:19:34 +0300

    feat(database): Integrate MongoDB connection, initialize collections, and refactor app bootstrap to support environment variables

--- src/settings/config.ts
+++ src/settings/config.ts
@@ -1,3 +1,7 @@
+import dotenv from 'dotenv';
+
+dotenv.config();
+
 const env = process.env;
 
 export const ADMIN_USERNAME = env.ADMIN_USERNAME || 'admin';
@@ -5,4 +9,9 @@ export const ADMIN_PASSWORD = env.ADMIN_PASSWORD || 'qwerty';
 
 export const SETTINGS = {
   PORT: env.PORT || 5001,
+  MONGO_URL:
+    env.MONGO_URL ||
+    env.MONGODB_URI ||
+    'mongodb://localhost:27017/ed-back-lessons-uber',
+  DB_NAME: env.DB_NAME || 'ed-back-lessons-uber',
 };
